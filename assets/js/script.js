b3ff9ce3f7b56ffdc1b92e948a796885062b6416
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-02-08 19:15:10 -0700

    fixed js gen now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // Assignment code here
 
-var lowerChara = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,t,u,v,w,x,y,z";
-var upperChara = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-var numberChara = "0,1,2,3,4,5,6,7,8,9";
-var specialChara = "!,@,#,$,%,^,&,*,(,),_,+,=,{,},[,],\<,>,?,~,";
-var allChara = "";
-var randomNumber = "";
-var typesArray = "";
+const lowerChara = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","t","u","v","w","x","y","z"];
+const upperChara = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const numberChara = ["0","1","2","3","4","5","6","7","8","9"];
+const specialChara = ["!","@","#","$","%","^","&","*","(",")","_","+","=","{","}","[","]","<",">","?","~"];
+let allChara = [];
+
+
 
 
 // Get references to the #generate element
@@ -14,7 +14,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); 
+  console.log(password)
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -32,72 +33,59 @@ var generatePassword = function () {
     generatePassword();
   }
 
-  var upperChara = window.confirm("Would you like uppercase characters included in your password?");
-  if (upperChara) {
+  var upperChar = window.confirm("Would you like uppercase characters included in your password?");
+  if (upperChar) {
     window.alert("Uppercase letters will be included in your password.");
-    allChara += upperChara;
+    allChara.push(...upperChara);
   }
   else {
     window.alert("No uppercase letters will be included your password.");
   }
 
-  var lowerChara = window.confirm("Would you like lowercase letters included in your password?");
-  if (lowerChara) {
+  var lowerChar = window.confirm("Would you like lowercase letters included in your password?");
+  if (lowerChar) {
     window.alert("Lowercase letters will be include in your password.");
-    allChara += lowerChara;
+    allChara.push(...lowerChara);
   }
   else {
     window.alert("No lowercase letters will be included in your password.");
   }
 
-  var numberChara = window.confirm("Would you like numbers included in your password?");
-  if (numberChara) {
+  var numberChar = window.confirm("Would you like numbers included in your password?");
+  if (numberChar) {
     window.alert("Numbers will be included in your password.");
-    allChara += numberChara;
+    allChara.push(...numberChara);
   }
   else {
     window.alert("No numbers will be included in your password.");
   }
 
-  var specialChara = window.confirm("Would you like special characters included in your password?");
-  if (specialChara) {
+  var specialChar = window.confirm("Would you like special characters included in your password?");
+  if (specialChar) {
     window.alert("Special characters will be included in your password.");
-    allChara += specialChara;
+    allChara.push(...specialChara);
   }
   else {
     window.alert("No special characters will be included in your password.");
   }
 
-  if (allChara) {
+  if (allChara.length) {
     window.alert("Generating your password now!");
   }
   else {
     window.alert("You must include at least one of the previous choices. No password generated.");
-    return;
+    return generatePassword;
   }
 
-
-      var temp = new Array();
+    var password = "";
       for (let i = 0; i < lengthChara; i++) {
         randomNumber = Math.floor(Math.random() * allChara.length);
-        numbArray = allChara[randomNumber];
-        temp.push(numbArray);
+        password += allChara[randomNumber];
       }  
        
-      return result.join("");
+      return password;
       
-}
-      
-      // Write password to the #password input
-      function writePassword() {
-        var password = generatePassword();
-        var passwordText = document.querySelector("#password");
-      
-        
-        passwordText.value = password;
-      }
-
-    
+}    
 
     // Add event listener to generate button
     generateBtn.addEventListener("click", writePassword);
